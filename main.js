9b2cb5f23cd614041aaf0fd8df6f31e19c94214d
Trindade-Rafael <86261398+Trindade-Rafael@users.noreply.github.com>
2024-05-06 22:00:42 -0300

    Funções e parÂmetros
    Construindo as funções

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-document.querySelector('.tecla_pom');
+
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+//document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+const listaDeTeclas =document.querySelectorAll('.tecla');
+let contador =0;
+
+
+while(contador <listaDeTeclas.length){
+    listaDeTeclas[contador].onclick =tocaSom;
+    contador =contador +1;
+    console.log(contador);
+}
+

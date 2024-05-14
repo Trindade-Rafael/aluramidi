b1d69260e2eff034339aed8fd3b8adadefdd60d3
Trindade-Rafael <86261398+Trindade-Rafael@users.noreply.github.com>
2024-05-14 00:27:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,18 +9,19 @@ const listaDeTeclas =document.querySelectorAll('.tecla');
 let contador =0;
 
 
-while(contador <listaDeTeclas.length){
-    const tecla = listaDeTeclas[contador];
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
 
+    const tecla = listaDeTeclas[contador];
     const instrumento =  tecla.classList[1];
-    console.log(instrumento);
     const idAudio = `#som_${instrumento}`;
-    console.log(idAudio);
 
     tecla.onclick = function(){
         tocaSom(idAudio);
     }
-    contador =contador +1;
-    console.log(contador);
+
+    tecla.onkeydown = function(){
+        tecla.classList.add('ativa');
+    }
+
 }
 
